3b680de468dcf83917ad97356e2ccf3ed63282dd
Feri Djuandi <38802248+feridj@users.noreply.github.com>
2021-05-28 11:33:23 +0700

    Add files via upload

---
+++ bab02/SpringHelloworld/src/main/java/com/tobuku/common/App.java
@@ -0,0 +1,21 @@
+package com.tobuku.common;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static ApplicationContext context;
+
+	public static void main(String[] args) {
+	      context = new ClassPathXmlApplicationContext("Beans.xml");
+
+	      Hello obj = (Hello) context.getBean("helloWorld");
+
+	      obj.printMessage();
+	   }
+}
---
+++ bab02/SpringHelloworld/src/main/java/com/tobuku/common/Hello.java
@@ -0,0 +1,6 @@
+package com.tobuku.common;
+
+public interface Hello {
+
+	public void printMessage();
+}
